a4b232eea3cab5358bfb01d3f2f3981826114bbe
herzorf <1446450047@qq.com>
2023-05-08 14:12:55 +0800

    添加路由组件

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,3 +1,7 @@
+import { First } from '../components/welcome/first.tsx'
+import { Forth } from '../components/welcome/forth.tsx'
+import { Second } from '../components/welcome/second.tsx'
+import { Third } from '../components/welcome/third.tsx'
 import { Bar } from '../view/bar.tsx'
 import { Foo } from '../view/foo.tsx'
 import { Welcome } from '../view/welcome/index.tsx'
@@ -5,6 +9,14 @@ import { Welcome } from '../view/welcome/index.tsx'
 export const routes = [
     { path: "/", component: Bar },
     { path: '/foo', component: Foo },
-    { path: '/welcome', component: Welcome },
+    {
+        path: '/welcome', component: Welcome,
+        children: [
+            { path: 'first', component: First },
+            { path: 'second', component: Second },
+            { path: 'third', component: Third },
+            { path: 'forth', component: Forth },
+        ]
+    },
 
 ]
--- src/view/welcome/index.tsx
+++ src/view/welcome/index.tsx
@@ -1,9 +1,12 @@
 import { defineComponent } from 'vue'
 import styles from "./index.module.scss"
+import { RouterView } from 'vue-router'
 export const Welcome = defineComponent({
     setup() {
         return () => (
-            <div class={styles.wrapper}>welcome</div>
+            <div class={styles.wrapper}>welcome
+                <RouterView></RouterView>
+            </div>
         )
     }
 })
