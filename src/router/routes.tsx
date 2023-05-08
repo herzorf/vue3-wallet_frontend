935535b622139c65bdbe7445f658ccc461363653
herzorf <1446450047@qq.com>
2023-05-08 16:42:20 +0800

    添加背景颜色

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,13 +2,9 @@ import { First } from '../components/welcome/first.tsx'
 import { Forth } from '../components/welcome/forth.tsx'
 import { Second } from '../components/welcome/second.tsx'
 import { Third } from '../components/welcome/third.tsx'
-import { Bar } from '../view/bar.tsx'
-import { Foo } from '../view/foo.tsx'
 import { Welcome } from '../view/welcome/index.tsx'
 
 export const routes = [
-    { path: "/", component: Bar },
-    { path: '/foo', component: Foo },
     {
         path: '/welcome', component: Welcome,
         children: [
