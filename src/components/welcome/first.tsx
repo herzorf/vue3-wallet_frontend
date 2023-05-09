8a8ecac400100d5b33d49c7de305ae15ca99218b
herzorf <1446450047@qq.com>
2023-05-09 15:49:53 +0800

    完成第一个页面

--- src/components/welcome/first.tsx
+++ src/components/welcome/first.tsx
@@ -1,8 +1,19 @@
 import { defineComponent } from 'vue'
+import styles from "./first.module.scss"
+import saving from "../../assets/icons/saving.svg"
 export const First = defineComponent({
     setup() {
         return () => (
-            <div>first</div>
+            <div class={styles.wrapper}>
+                <div class="card">
+                    <img src={saving} alt="" />
+                </div>
+                <div class="footNav">
+                    <router-link class="fake" to="/welcome/second">下一页</router-link>
+                    <router-link to="/welcome/second">下一页</router-link>
+                    <router-link to="/welcome/second">跳过</router-link>
+                </div>
+            </div>
         )
     }
 })
