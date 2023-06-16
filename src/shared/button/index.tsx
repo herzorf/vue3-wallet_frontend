e4b49d051c3ff8234c92a794d10a19d811e8b41c
herzorf <1446450047@qq.com>
2023-06-16 10:57:51 +0800

    添加button组件

---
+++ src/shared/button/index.tsx
@@ -0,0 +1,14 @@
+import { defineComponent } from 'vue'
+import styles from "./index.module.scss"
+interface PropsType {
+    onClick: (e: MouseEvent) => void
+}
+export const Button = defineComponent<PropsType>({
+    setup(_, context) {
+        return () => (
+            <button class={styles.button}>
+                {context.slots.default?.()}
+            </button>
+        )
+    }
+})
--- src/view/start/index.tsx
+++ src/view/start/index.tsx
@@ -1,8 +1,14 @@
 import { defineComponent } from 'vue'
+import { Button } from '../../shared/button'
+import styles from "./index.module.scss"
 export const Start = defineComponent({
     setup() {
         return () => (
-            <div>start</div>
+            <div class={styles.wrapper}>
+                <div class="button-wrapper">
+                    <Button class="button" onClick={() => { console.log(111) }}>测试</Button>
+                </div>
+            </div>
         )
     }
 })
