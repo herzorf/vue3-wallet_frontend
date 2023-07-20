59789f342cc91fbcacb202399e2c14f43bbe1f85
herzorf <1446450047@qq.com>
2023-07-20 16:16:57 +0800

    wip: 实现item/create的mainlayout部份

--- src/components/items/itemCreate/index.tsx
+++ src/components/items/itemCreate/index.tsx
@@ -1,8 +1,22 @@
 import { defineComponent } from 'vue'
+import { MainLayout } from '../../../shared/mainLayout'
+import { Icon } from '../../../shared/icon'
+import styles from "./index.module.scss"
 export const ItemCreate = defineComponent({
     setup() {
         return () => (
-            <div>ItemCreate</div>
+            <div class={styles.wrapper}>
+                <MainLayout>
+                    {{
+                        title: () => <span>记一笔</span>,
+                        icon: () => <Icon class="icon" name='left' />,
+                        default: () => (
+                            <div>main</div>
+                        )
+                    }}
+                </MainLayout>
+            </div>
+
         )
     }
 })
--- src/view/items/index.tsx
+++ src/view/items/index.tsx
@@ -1,12 +1,9 @@
 import { defineComponent } from 'vue'
-import styles from "./index.module.scss"
 import { RouterView } from 'vue-router'
 export const Items = defineComponent({
     setup() {
         return () => (
-            <div class={styles.wrapper}>
-                <RouterView />
-            </div>
+            <RouterView />
         )
     }
 })
